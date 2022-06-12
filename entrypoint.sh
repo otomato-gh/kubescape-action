6550ae20ff7b400cbc319c227ca0d3abfa78a1c2
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2022-06-12 16:24:28 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eax
+
+kubescape --version 
+
+# checking whether the threshold is empty or not 
+
+if test -z "$3"
+then 
+kubescape scan framework nsa $2 *.yaml -f $4
+else 
+kubescape scan framework nsa $2 *.yaml -t $3 -f $4 
+fi 
