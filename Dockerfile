8ef548c41bba93664d243170c0807bc59736ef71
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2022-06-18 16:43:39 +0300

    Update entrypoint.sh
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update action.yaml
    
    Update action.yaml
    
    Update action.yaml
    
    Update test.yaml
    
    Update action.yaml
    
    Update entrypoint.sh
    
    Update Dockerfile
    
    Update entrypoint.sh
    
    Update Dockerfile
    
    Update test.yaml
    
    Update entrypoint.sh
    
    Update Dockerfile
    
    Update test.yaml
    
    Update Dockerfile
    
    Update README.md

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,11 @@
 FROM ubuntu:20.04
 
-ARG kubescape-version
-ENV KUBESCAPE_VERSION=$kubescape-version
 ENV KUBESCAPE_SKIP_UPDATE_CHECK 1
-
-RUN apt-get -qq update && apt-get -qq -y install apt-utils curl
-
-RUN curl -sL -o /usr/bin/kubescape \
-    https://github.com/armosec/kubescape/releases/download/${KUBESCAPE_VERSION}/kubescape-ubuntu-latest
-
-RUN chmod +x /usr/bin/kubescape
+RUN apt-get update && apt-get install -y curl
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
+
+#ENTRYPOINT [ "kubescape" ]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 set -eax
 
+echo "https://github.com/armosec/kubescape/releases/download/$1/kubescape-ubuntu-latest"
+curl -sL -o /usr/bin/kubescape \
+    https://github.com/armosec/kubescape/releases/download/$1/kubescape-ubuntu-latest
+    
+chmod +x /usr/bin/kubescape
+
 kubescape --version 
 
 # checking whether the threshold is empty or not 
 
 if test -z "$3"
 then 
-kubescape scan framework nsa $2 *.yaml -f $4
+kubescape scan framework nsa $2 -f $4
 else 
-kubescape scan framework nsa $2 *.yaml -t $3 -f $4 
+kubescape scan framework nsa $2 -t $3 -f $4 
 fi 
