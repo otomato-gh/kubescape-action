cd6db32f46df8ffa1f6097aeb8f9a39157fa052d
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2022-06-18 20:25:56 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -eax
 echo "https://github.com/armosec/kubescape/releases/download/${{ inputs.ksversion }}/kubescape-ubuntu-latest"
 curl -sL -o /usr/bin/kubescape \
     https://github.com/armosec/kubescape/releases/download/$1/kubescape-ubuntu-latest
+    
+chmod +x /usr/bin/kubescape
 
 kubescape --version 
 
