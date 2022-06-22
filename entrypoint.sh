71cf30379a5d0366685e722e598c214c8bcd4182
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2022-06-23 00:12:03 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,13 @@ kubescape --version
 
 # specifying output file to save results to
 
-if [ "$4" == "json" ]
+if [ $4 == "json" ]
 then  
   saveto="--format-version v2 --output results.json"
-elif [ "$4" == "junit" ] 
+elif [ $4 == "junit" ] 
 then 
   saveto="--output results.xml"
-elif [ "$4" == "pdf" ] 
+elif [ $4 == "pdf" ] 
 then 
   saveto="--output results.pdf"
 fi
