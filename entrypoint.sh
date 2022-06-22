fb3e1d5fa0c1f585b70094918e0d142036850519
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2022-06-22 23:54:57 +0300

    Specifying output file to save results to (depends on format)

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,24 @@ chmod +x /usr/bin/kubescape
 
 kubescape --version 
 
+# specifying output file to save results to
+
+if [ "$4" == "json" ]
+then  
+  saveto="--format-version v2 --output results.json"
+elif [ "$4" == "junit" ] 
+then 
+  saveto="--output results.xml"
+elif [ "$4" == "pdf" ] 
+then 
+  saveto="--output results.pdf"
+fi
+
 # checking whether the threshold is empty or not 
 
 if test -z "$3"
 then 
-kubescape scan framework nsa $2 -f $4
+kubescape scan framework nsa $2 -f $4 $saveto
 else 
-kubescape scan framework nsa $2 -t $3 -f $4 
+kubescape scan framework nsa $2 -t $3 -f $4 $saveto
 fi 
