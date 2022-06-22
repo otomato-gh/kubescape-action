c753aec183456ce3c1d4e6f7217dd04e58809434
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2022-06-23 00:12:03 +0300

    Update test.yaml
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update test.yaml
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Added timestamp to results' filename
    
    Update test.yaml
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Removed file timestamps
    
    Update Dockerfile
    
    Update entrypoint.sh
    
    Update action.yaml
    
    Update entrypoint.sh
    
    Update action.yaml
    
    Update action.yaml
    
    Update action.yaml
    
    Update Dockerfile
    
    Update action.yaml
    
    Update entrypoint.sh
    
    Update action.yaml
    
    Update action.yaml
    
    Update entrypoint.sh
    
    Update action.yaml
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update action.yaml
    
    Adding artifact retrieval

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eax
 
 echo "https://github.com/armosec/kubescape/releases/download/$1/kubescape-ubuntu-latest"
@@ -9,24 +9,27 @@ chmod +x /usr/bin/kubescape
 
 kubescape --version 
 
-# specifying output file to save results to
+echo "Output format is set to: $4"
 
-if [ "$4" == "json" ]
-then  
-  saveto="--format-version v2 --output results.json"
-elif [ "$4" == "junit" ] 
-then 
-  saveto="--output results.xml"
-elif [ "$4" == "pdf" ] 
-then 
-  saveto="--output results.pdf"
+if [[ "$4" == "json" ]]; then  
+export savetostr="--format-version v2 --output results.json"
+echo "Output format is set to: $4, so we'll save the results with $savetostr"
+elif [[ "$4" == "junit" ]]; then 
+export savetostr="--output results.xml"
+echo "Output format is set to: $4, so we'll save the results with $savetostr"
+elif [[ "$4" == "pdf" ]]; then 
+export savetostr="--output results.pdf"
+echo "Output format is set to: $4, so we'll save the results with $savetostr"
 fi
 
 # checking whether the threshold is empty or not 
 
 if test -z "$3"
 then 
-kubescape scan framework nsa $2 -f $4 $saveto
+kubescape scan framework nsa $2 -f $4 $savetostr
 else 
-kubescape scan framework nsa $2 -t $3 -f $4 $saveto
-fi 
+kubescape scan framework nsa $2 -t $3 -f $4 $savetostr
+fi
+
+
+
