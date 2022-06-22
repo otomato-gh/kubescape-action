4df6a569cbd5ecd6da52a9c497404db7fabc373a
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2022-06-23 00:46:22 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,22 +11,22 @@ kubescape --version
 
 # specifying output file to save results to
 
-if [ $4 == "json" ]
-then  
-  saveto="--format-version v2 --output results.json"
-elif [ $4 == "junit" ] 
-then 
-  saveto="--output results.xml"
-elif [ $4 == "pdf" ] 
-then 
-  saveto="--output results.pdf"
-fi
+#if [ $4 == "json" ]
+#then  
+#  saveto="--format-version v2 --output results.json"
+#elif [ $4 == "junit" ] 
+#then 
+#  saveto="--output results.xml"
+#elif [ $4 == "pdf" ] 
+#then 
+#  saveto="--output results.pdf"
+#fi
 
 # checking whether the threshold is empty or not 
 
 if test -z "$3"
 then 
-kubescape scan framework nsa $2 -f $4 $saveto
+kubescape scan framework nsa $2 -f $4 $SAVETO
 else 
-kubescape scan framework nsa $2 -t $3 -f $4 $saveto
+kubescape scan framework nsa $2 -t $3 -f $4 $SAVETO
 fi 
