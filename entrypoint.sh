b22422aec99c79797c78413f6db49c7cd708e6ce
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2022-06-23 17:19:16 +0300

    Removed file timestamps

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,19 +9,16 @@ chmod +x /usr/bin/kubescape
 
 kubescape --version 
 
-# specifying output file to save results to`
-export filetimestamp =`date +"%Y-%m-%d-%H-%M-%S-%Z"`
-
 echo "Output format is set to: $4"
 
 if [[ "$4" == "json" ]]; then  
-export savetostr="--format-version v2 --output results-$filetimestamp.json"
+export savetostr="--format-version v2 --output results.json"
 echo "Output format is set to: $4, so we'll save the results with $savetostr"
 elif [[ "$4" == "junit" ]]; then 
-export savetostr="--output results-$filetimestamp.xml"
+export savetostr="--output results.xml"
 echo "Output format is set to: $4, so we'll save the results with $savetostr"
 elif [[ "$4" == "pdf" ]]; then 
-export savetostr="--output results-$filetimestamp.pdf"
+export savetostr="--output results.pdf"
 echo "Output format is set to: $4, so we'll save the results with $savetostr"
 fi
 
