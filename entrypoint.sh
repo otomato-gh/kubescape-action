d911d541825fb18766d0c371574a3d91553e8111
Roman Balashevich <65680726+rbalashevich@users.noreply.github.com>
2022-06-23 17:14:52 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,18 +10,18 @@ chmod +x /usr/bin/kubescape
 kubescape --version 
 
 # specifying output file to save results to`
-export filetimestamp =`date +"%Y_%m_%d_%H_%M_%S"`
+export filetimestamp =`date +"%Y-%m-%d-%H-%M-%S-%Z"`
 
 echo "Output format is set to: $4"
 
 if [[ "$4" == "json" ]]; then  
-export savetostr="--format-version v2 --output results_$filetimestamp.json"
+export savetostr="--format-version v2 --output results-$filetimestamp.json"
 echo "Output format is set to: $4, so we'll save the results with $savetostr"
 elif [[ "$4" == "junit" ]]; then 
-export savetostr="--output results_$filetimestamp.xml"
+export savetostr="--output results-$filetimestamp.xml"
 echo "Output format is set to: $4, so we'll save the results with $savetostr"
 elif [[ "$4" == "pdf" ]]; then 
-export savetostr="--output results_$filetimestamp.pdf"
+export savetostr="--output results-$filetimestamp.pdf"
 echo "Output format is set to: $4, so we'll save the results with $savetostr"
 fi
 
